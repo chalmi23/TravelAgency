d5f1be3870508c304e35c7a9c5fe1e5170fb869c
chalmi23 <michalekxdpl23@gmail.com>
2023-05-04 19:49:31 +0200

    poprawki

--- biuropodrozyprojekt/searchUserControl.cs
+++ biuropodrozyprojekt/searchUserControl.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.Sql;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.IO;
@@ -42,7 +37,6 @@ namespace biuropodrozyprojekt
             dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Century Gothic", 10, FontStyle.Bold);
             dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
 
-            string search = searchTx.Text;
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
 
@@ -50,7 +44,7 @@ namespace biuropodrozyprojekt
                 SqlDataAdapter adapter = new SqlDataAdapter();
                 SqlCommand command = new SqlCommand("SELECT DISTINCT Vacation.VacationId, TypeOfVacation.TypeName AS 'Trip type', Country.Country, CountryCity.City, Vacation.HotelName AS 'Hotel Name', Vacation.HotelRating AS 'Hotel Rating', Vacation.MaxPeople AS 'Free places', VehicleType.VehicleName AS 'Type of Vehicle', Vacation.Price AS 'Price($)', DateVacation.DepartureDate AS Departure, DateVacation.ArrivalDate as Arrival  FROM Vacation INNER JOIN DateVacation ON DateVacation.VacationId = Vacation.VacationId INNER JOIN Country ON Country.CountryId = Vacation.CountryId INNER JOIN TypeOfVacation ON TypeOfVacation.TypeId = Vacation.TypeId INNER JOIN VehicleType ON VehicleType.VehicleId = Vacation.VehicleId INNER JOIN CountryCity ON CountryCity.CityId = Vacation.CityId WHERE Country.Country LIKE '%' + @search + '%' OR CountryCity.City LIKE '%' + @search + '%' OR Vacation.HotelName LIKE '%' + @search + '%' OR Vacation.HotelRating LIKE '%' + @search + '%' OR DateVacation.ArrivalDate LIKE '%' + @search + '%' OR DateVacation.DepartureDate LIKE '%' + @search + '%'  OR VehicleType.VehicleName LIKE '%' + @search + '%' OR Vacation.Price LIKE '%' + @search + '%' OR TypeOfVacation.TypeName LIKE '%' + @search + '%'", connection);
 
-                command.Parameters.AddWithValue("@search", search);
+                command.Parameters.AddWithValue("@search", searchTx.Text);
                 adapter.SelectCommand = command;
 
                 DataTable dataTable = new DataTable();
